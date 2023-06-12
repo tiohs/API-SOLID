dd0468286fae911ad819b124bad30e355d153652
Hamilton Silva <hamiltonsilva551@gmail.com>
2023-06-12 15:05:03 +0100

    Configuração do prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { date } from 'zod'
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Hamilton Silva',
+    email: 'hamiltonsilva.dev@gmail.com',
+    password_hash: 'sdfweg43wefsd32',
+  },
+})
