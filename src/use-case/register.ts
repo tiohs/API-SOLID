ff4203e3b497d813215038f08c5d5eb14949826d
Hamilton Silva <hamiltonsilva551@gmail.com>
2023-07-03 17:16:51 +0100

    Create Repositores to Register,  implementation caseUSe

---
+++ src/use-case/register.ts
@@ -0,0 +1,32 @@
+import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prrisma-users-repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+export async function RegisterCasoUse({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('E-mail already exists')
+  }
+  const prismaUsersRepository = new PrismaUsersRepository()
+  prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  })
+}
